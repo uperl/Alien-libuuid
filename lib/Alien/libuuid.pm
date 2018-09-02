c46291312c5b3508839b0d65062b8bd2a993d4c2
Graham Ollis <plicease@cpan.org>
2018-09-02 17:08:54 -0400

    Initial commit

---
+++ lib/Alien/libuuid.pm
@@ -0,0 +1,11 @@
+package Alien::libuuid;
+
+use strict;
+use warnings;
+use 5.008001;
+use base qw( Alien::Base );
+
+# ABSTRACT: Find or download and install libuuid
+# VERSION
+
+1;
---
+++ t/alien_libuuid.t
@@ -0,0 +1,6 @@
+use Test2::V0 -no_srand => 1;
+use Alien::libuuid;
+
+ok 1, 'todo';
+
+done_testing
